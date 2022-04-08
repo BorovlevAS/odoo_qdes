3dc4a7c15db0bc69f2b62f3210d239693c9704d9
Artem Borovlev <bas@qdes.com.ua>
2022-04-08 18:25:59 +0300

    biko icons addon

---
+++ biko_apss_icons/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Icons for Apps",
+    "description": """Minimalist and elegant icons for apps""",
+    "category": "Themes/Backend",
+    "version": "14.0.1.1.1",
+    'author': 'BIKO Solutions',
+    'company': 'BIKO Solutions',
+    'maintainer': 'BIKO Solutions',
+    "depends": ['base'],
+    "data": [
+        'views/icons.xml',
+    ],
+    'license': 'LGPL-3',
+    'pre_init_hook': 'test_pre_init_hook',
+    'post_init_hook': 'test_post_init_hook',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
