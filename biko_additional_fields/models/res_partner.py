ba5d19cb95964fda39b89dd7ad02ff9da440c9aa
Artem Borovlev <bas@qdes.com.ua>
2022-04-12 01:04:06 +0300

    [+] второй email, telegram, skype

--- biko_additional_fields/models/res_partner.py
+++ biko_additional_fields/models/res_partner.py
@@ -10,3 +10,11 @@ class ResPartner(models.Model):
 
     source_id = fields.Many2one(comodel_name = 'biko.source', string = 'Source')
     recomendator_id = fields.Many2one(comodel_name = 'res.partner', string = 'Recommendator')
+
+    email2 = fields.Char()
+    email2_formatted = fields.Char(
+        'Additional Email', compute='_compute_email_formatted',
+        help='Format email address "Name <email@domain>"')
+
+    telegram = fields.Char(string = 'Telegram')
+    skype = fields.Char(string='Skype')
